a8df7acc0977425b51920a602d9fe656f77bc242
YoursTruly450 <yourstruly.bill@gmail.com>
2021-12-29 14:47:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+let data = {
+  price: 5,
+  quantity: 2
+}
+
+let target, total, salePrice
+
+class Dep {
+  constructor() {
+    this.subscribers = [];
+  }
+
+  depend() {
+    if(target && !this.subscribers.includes(target)) {
+      this.subscribers.push(target);
+    }
+  }
+
+  notify() {
+    this.subscribers.forEach(sub => sub());
+  }
+}
+
+Object.keys(data).forEach(key => {
+  let internalValue = data[key];
+
+  const dep = new Dep();
+
+  Object.defineProperty(data, key, {
+    get() {
+      dep.depend();
+      return internalValue;
+    },
+    set(newValue) {
+      internalValue = newValue;
+      dep.notify();
+    }
+  })
+})
+
+function watcher(myFunc) {
+  target = myFunc;
+  target();
+  target = null;
+}
+
+watcher(() => {
+  total = data.price * data.quantity;
+})
+
+watcher(() => {
+  salePrice = data.price * 0.9;
+})
